68209d97e9c0ff8abe3db98a5200ee572715ca53
Yeremeev <tonaros.kill@gmail.com>
2020-02-12 12:42:58 +0200

    a separate class for constants was created

---
+++ App/Constants/ConstantProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Constants
+{
+    public class ConstantProvider
+    {
+        public const int exifParamSize = 8;
+        public const char downloadMode = '1';
+        public const string FILE_DOWNLOADING_FAILED = "Downloading of file failed: ";
+        public const string FILE_DELETION_FAILED = "Deletion of file failed: ";
+
+    }
+}
