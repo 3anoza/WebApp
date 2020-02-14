0336059d368684e31f10392b6b548be5fd5908e4
Yeremeev <tonaros.kill@gmail.com>
2020-02-14 09:48:48 +0200

    created a new class to set default settings

---
+++ App/Core/DefaultConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+using App.Constants;
+
+namespace App.Core
+{
+    public class DefaultConfig
+    {
+        public void setDefaultAppSettings()
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = config.AppSettings.Settings;
+            settings.Add(ConstantProvider.serverUploadDirectory, ConstantProvider.defaultDirectory);
+            settings.Add(ConstantProvider.fileTypes,ConstantProvider.defaultTypes);
+            ViewBag.LOL = "LOL";
+        }
+        
+        // if config not found or empty -> set default value
+    }
+}
