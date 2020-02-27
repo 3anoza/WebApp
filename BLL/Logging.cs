0d2f74659b4ec14fc1c4b145ef7a5c5848af6f38
Yeremeev <tonaros.kill@gmail.com>
2020-02-27 13:37:27 +0200

    a business layer was added as a library

---
+++ BLL/Logging.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace App.Core
+{
+    public class Logging
+    {
+        Logging(string message)
+        {
+            using (System.IO.FileStream stream = new System.IO.FileStream("C:/Users/%User%/.debug/debug.msg",System.IO.FileMode.CreateNew,System.IO.FileAccess.Write))
+            {
+                stream.Write(System.Text.Encoding.UTF8.GetBytes(message), 0, message.Length);
+            }
+        }
+    }
+}
