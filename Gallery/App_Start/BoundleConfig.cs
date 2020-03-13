eb6ae94f6fafbd2c5f120e822813637996e4a855
Yeremeev <tonaros.kill@gmail.com>
2020-03-13 17:45:12 +0200

    (in one push) the main project was recreated, styles, pages added, work on authorization started

---
+++ Gallery/App_Start/BoundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Gallery
+{
+    public class BoundleConfig
+    {
+        public static void RegisterBoundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundless/bootstrap").Include(
+                "~/Scripts/boostrap.js"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/Site.css"));
+        }
+    }
+}
