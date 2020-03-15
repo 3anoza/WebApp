5240565770b5461b0effdbe55976bd79add6a853
Yeremeev <tonaros.kill@gmail.com>
2020-03-15 19:14:33 +0200

    added the ability to set default or set your location of images and their format

---
+++ Gallery.DAL/ConfigManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+
+namespace Gallery.DAL
+{
+    public class ConfigManager
+    {
+        public ConstantProvider Constants = new ConstantProvider();
+        public void SetDefaultImageProperty()
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = config.AppSettings.Settings;
+            settings.Add("ImageDirectory", Constants.DefaultPathToImage);
+            settings.Add("FileType",Constants.DefaultImageTypes);
+        }
+
+        public bool SetImageProperty()
+        {
+            return false;
+        }
+
+        public string[] GetImageProperty()
+        {
+            return null;
+        }
+
+    }
+}
