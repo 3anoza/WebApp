c4505b49b7f1a6c6d62795cfb61816d1691ea754
Yeremeev <tonaros.kill@gmail.com>
2020-03-15 19:05:29 +0200

    image class has been updated: EXIF ​​output has been added

--- Gallery.BLL/SimilaritiesImages.cs
+++ Gallery.BLL/SimilaritiesImages.cs
@@ -1,11 +1,15 @@
-﻿using System.Web;
+﻿using System;
+using System.Configuration;
+using System.Web;
 using System.IO;
-
+using System.Windows.Media.Imaging;
+using Gallery.DAL;
 
 namespace Gallery.BLL
 {
     public class SimilaritiesImages
     {
+        public ConstantProvider Constants { get; } = new ConstantProvider();
         /// <summary>
         /// Will return logic value (<typeparamref name="true"/> or <typeparamref name="false"/>) depending on whether there is such a hash in the database
         /// </summary>
@@ -40,7 +44,7 @@ namespace Gallery.BLL
             {
                 string imageName = Path.GetFileName(pathToImages);
                 FileInfo information = new FileInfo(imageName);
-                if (MimeMapping.GetMimeMapping(imageName) == allowedFormat)
+                if (MimeMapping.GetMimeMapping(imageName) == ConfigurationManager.AppSettings.Get("FileType"))
                 {
                     if (information.Length > 0)
                     {
@@ -62,6 +66,72 @@ namespace Gallery.BLL
             }
 
             return status;
+        }
+
+        public string[] GetExifDataStrings(string pathToImage)
+        {
+            
+            string[] exif = new string[Constants.InfoSize];
+            FileInfo info = new FileInfo(pathToImage);
+            using (FileStream fileStream = new FileStream(pathToImage, FileMode.Open))
+            {
+                BitmapSource img = BitmapFrame.Create(fileStream);
+                BitmapMetadata metadata = (BitmapMetadata)img.Metadata;
+                if (pathToImage.Contains("png"))
+                {
+                    for (byte i = 0; i < 4; i++)
+                    {
+                        exif[i] = "Empty";
+                    }
+                }
+                else
+                {
+                    if (metadata.Title == null)
+                    {
+                        exif[0] = "Empty";
+                    }
+                    else
+                    {
+                        exif[0] = metadata.Title.ToString();
+                    }
+                    // check camera creator from metadata
+                    if (metadata.CameraManufacturer == null)
+                    {
+                        exif[1] = "Empty";
+                    }
+                    else
+                    {
+                        exif[1] = metadata.CameraManufacturer.ToString();
+                    }
+                    // check camera model from metadata
+                    if (metadata.CameraModel == null)
+                    {
+                        exif[2] = "Empty";
+                    }
+                    else
+                    {
+                        exif[2] = metadata.CameraModel.ToString();
+                    }
+                    // check file data maken from metadata
+                    if (metadata.DateTaken == null)
+                    {
+                        exif[3] = "Empty";
+                    }
+                    else
+                    {
+                        exif[3] = metadata.DateTaken.ToString();
+                    }
+                }
+                exif[4] = img.PixelWidth.ToString();
+                exif[5] = img.PixelHeight.ToString();
+                // convert file size from byte to Kbyte
+                double KB = (double)info.Length / 1024;
+                exif[6] = Math.Round(KB, 2).ToString();
+                // save filename
+                exif[7] = info.Name.ToString();
+            }
+
+            return exif;
         }
     }
 }
