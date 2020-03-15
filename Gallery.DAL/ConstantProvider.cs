bc06776c56fbb9240bd232dac361fda4c9710bf0
Yeremeev <tonaros.kill@gmail.com>
2020-03-15 19:16:06 +0200

    added a class that stores property

---
+++ Gallery.DAL/ConstantProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Gallery.DAL
+{
+    public class ConstantProvider
+    {
+        public byte InfoSize { get;} = 8;
+        public string DefaultPathToImage { get; } = "~/Assets/Image";
+        public string DefaultImageTypes { get; } = "image/jpeg;image/png";
+    }
+}
