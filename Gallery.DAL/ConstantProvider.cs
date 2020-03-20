96d3b1b1355102c72b1d30f2d7330db15914de51
Yeremeev <tonaros.kill@gmail.com>
2020-03-20 17:44:22 +0200

    moved to a new directory under a new name

--- Gallery.DAL/ConstantProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Gallery.DAL
-{
-    public class ConstantProvider
-    {
-        public byte InfoSize { get;} = 8;
-        public string DefaultPathToImage { get; } = "~/Assets/Image";
-        public string DefaultImageTypes { get; } = "image/jpeg;image/png";
-    }
-}
