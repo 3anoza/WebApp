0f380d9e609bb1e58596dd9d143262644ed21673
Yeremeev <tonaros.kill@gmail.com>
2020-03-20 17:40:09 +0200

    added class with properties

---
+++ Gallery.DAL/ProjectProperties/Properties.cs
@@ -0,0 +1,9 @@
+﻿namespace Gallery.DAL.ProjectProperties
+{
+    public class Properties
+    {
+        public byte ExifArraySize { get; private set; } = 8;
+        public string DefaultPathToImage { get; private set; } = "~/Resources/Images";
+        public string DefaultImageTypes { get; private set; } = "image/jpeg;image/png";
+    }
+}
