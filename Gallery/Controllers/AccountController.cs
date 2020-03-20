b0d87c3927caed4667e49ff230ec5c23a20b1ad9
Yeremeev <tonaros.kill@gmail.com>
2020-03-20 17:09:21 +0200

    the old controller is changed, at the moment it does not perform any tasks

---
+++ Gallery/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gallery.Controllers
+{
+    public class AccountController : Controller
+    {
+        protected string[] Mail { get; } = new string[] {"@mail.ru","@mail.ua","@list.ru"};
+        // GET: Auth
+        public ActionResult Register(FormCollection collection)
+        {
+            /*string[] value = new string[collection.Count];
+            for (int i = 0; i < collection.Count; i++)
+            {
+                value[i] = collection.Get(i);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (value[i].Contains(Mail[i]))
+                    break;
+            }
+            // send email to (LocalDB)
+            if (value[1].Contains(value[2]) && value[1].Length == value[2].Length)
+            {
+                //send password to (LocalDB)
+            }*/
+            return View();
+        }
+
+        public ActionResult _ViewTest()
+        {
+            return View();
+        }
+    }
+}
