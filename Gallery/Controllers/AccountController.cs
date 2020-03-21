5015464850f4feb254c900ba19ae3b416e246c67
Yeremeev <tonaros.kill@gmail.com>
2020-03-21 13:56:58 +0200

    added empty post and get methods for authentication and authorization

--- Gallery/Controllers/AccountController.cs
+++ Gallery/Controllers/AccountController.cs
@@ -4,37 +4,37 @@ using System.Linq;
 using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using Gallery.DAL.Models;
 
 namespace Gallery.Controllers
 {
     public class AccountController : Controller
     {
-        protected string[] Mail { get; } = new string[] {"@mail.ru","@mail.ua","@list.ru"};
-        // GET: Auth
-        public ActionResult Register(FormCollection collection)
+        // GET: Register
+        public ActionResult Register()
         {
-            /*string[] value = new string[collection.Count];
-            for (int i = 0; i < collection.Count; i++)
-            {
-                value[i] = collection.Get(i);
-            }
-
-            for (int i = 0; i < 3; i++)
-            {
-                if (value[i].Contains(Mail[i]))
-                    break;
-            }
-            // send email to (LocalDB)
-            if (value[1].Contains(value[2]) && value[1].Length == value[2].Length)
-            {
-                //send password to (LocalDB)
-            }*/
             return View();
         }
+        // POST: Register
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Register(RegisterModel model)
+        {
 
-        public ActionResult _ViewTest()
+            return View(model);
+        }
+        // GET: Login
+        public ActionResult Login()
         {
             return View();
+        }
+        // POST: Login
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Login(LoginModel model)
+        {
+
+            return View(model);
         }
     }
 }
