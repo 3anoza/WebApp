80f0bf605119eb65d5f49a90285da888a7caf04e
Yeremeev <tonaros.kill@gmail.com>
2020-03-23 17:26:34 +0200

    added a class that stores the implementation of the IRepository interface methods

---
+++ Gallery.DAL/DbRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Gallery.DAL.Interfaces;
+using Gallery.DAL.Models;
+
+namespace Gallery.DAL
+{
+    public class DbRepository : IRepository
+    {
+        protected readonly UserContext Context = new UserContext();
+
+        public DbRepository(UserContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<bool> IsUserExistAsync(string username, string plainPassword)
+        {
+            return await Context.Users.AnyAsync(u =>
+                u.Email == username.Trim().ToLower() && u.Password == plainPassword.Trim());
+        }
+
+        public async Task AddUserToDatabaseAsync(string username, string plainPassword)
+        {
+            Context.Users.Add(new User() {Email = username, Password = plainPassword});
+            Context.SaveChanges();
+        }
+
+        public int GetPersonId(string username)
+        {
+            return Context.Users.Where(u => u.Email == username).Select(u => u.Id).FirstOrDefault();
+        }
+    }
+}
