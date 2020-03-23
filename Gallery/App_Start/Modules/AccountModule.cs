e59da34d4e78a1341bf554dfdaef063e631c4efd
Yeremeev <tonaros.kill@gmail.com>
2020-03-23 17:05:23 +0200

    added module for accountcontroller in which all types are currently registered

---
+++ Gallery/App_Start/Modules/AccountModule.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Gallery.BLL.Interfaces;
+using Gallery.BLL.Services;
+using Gallery.DAL;
+using Gallery.DAL.Interfaces;
+using Gallery.DAL.Models;
+
+namespace Gallery.Modules
+{
+    public class AccountModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UserContext>()
+                .AsSelf();
+            builder.RegisterType<DbRepository>()
+                .As<IRepository>();
+            builder.RegisterType<UserService>()
+                .As<IUserService>();
+
+            builder.RegisterType<AuthenticationService>()
+                .As<IAuthenticationService>();
+        }
+    }
+}
