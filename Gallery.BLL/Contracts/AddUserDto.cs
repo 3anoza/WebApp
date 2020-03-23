75e6a19af5f5ae0b0c6999c8a3427bd50717b32a
Yeremeev <tonaros.kill@gmail.com>
2020-03-23 17:23:34 +0200

    implementation of contracts for adding a user and storing a user is added

---
+++ Gallery.BLL/Contracts/AddUserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Gallery.BLL.Contracts
+{
+    public class AddUserDto
+    {
+        public string Username { get; }
+        public string PlainPassword { get; }
+        public AddUserDto(string username, string plainPassword)
+        {
+            Username = username;
+            PlainPassword = plainPassword;
+        }
+    }
+}
---
+++ Gallery.BLL/Contracts/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Gallery.BLL.Contracts
+{
+    public class UserDto
+    {
+        public int PersonID { get; set; }
+        public string Username { get; set; }
+    }
+}
