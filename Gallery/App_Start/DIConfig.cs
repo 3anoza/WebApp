c058e3b17c766959281712ab325a3abf959c6318
Yeremeev <tonaros.kill@gmail.com>
2020-03-23 17:09:40 +0200

    Added DI container implementation in DIConfig file

---
+++ Gallery/App_Start/DIConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Gallery.Modules;
+
+namespace Gallery
+{
+    public class DIConfig
+    {
+        public static void Configure(HttpConfiguration configuration)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            builder.RegisterModule<AccountModule>();
+            builder.RegisterModule<HomeModule>();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
