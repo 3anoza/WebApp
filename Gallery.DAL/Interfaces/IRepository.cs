a59c778962c0ab7d158f34a5113bf802a2327ec9
Yeremeev <tonaros.kill@gmail.com>
2020-03-23 17:27:53 +0200

    Added implementation of the IRepository interface

---
+++ Gallery.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Gallery.DAL.Interfaces
+{
+    public interface IRepository
+    {
+        Task<bool> IsUserExistAsync(string username, string plainPassword);
+        Task AddUserToDatabaseAsync(string username, string plainPassword);
+        int GetPersonId(string username);
+    }
+}
