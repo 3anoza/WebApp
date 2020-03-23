5ca1c3eab5d35443ee4aba6204ae5c4521eb8418
Yeremeev <tonaros.kill@gmail.com>
2020-03-23 17:06:00 +0200

    added module for homecontroller in which all types are currently registered

---
+++ Gallery/App_Start/Modules/HomeModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Gallery.BLL.Interfaces;
+using Gallery.BLL.Services;
+
+namespace Gallery.Modules
+{
+    public class HomeModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ImageService>()
+                .As<IImageService>();
+
+            builder.RegisterType<HashService>()
+                .As<IHashService>();
+        }
+    }
+}
