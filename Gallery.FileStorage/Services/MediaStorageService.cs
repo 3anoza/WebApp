859d1aa331972e7e7e5703aba32c05683c32ad7c
Yeremeev <tonaros.kill@gmail.com>
2020-05-19 14:08:29 +0300

    Media storage class added, but not implemented

---
+++ Gallery.FileStorage/Services/MediaStorageService.cs
@@ -0,0 +1,25 @@
+﻿namespace Gallery.FileStorage.Services
+{
+    public class MediaStorageService : FileStorage.Interfaces.IMediaStorage 
+    {
+        public bool IsExist(byte[] bytes)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Create(byte[] bytes)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(byte[] bytes)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(byte[] bytes)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
