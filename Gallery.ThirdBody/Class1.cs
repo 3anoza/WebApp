631d7a08b63d256b4cd43a6102a9ef1c977479eb
Yeremeev <tonaros.kill@gmail.com>
2020-05-19 14:10:29 +0300

    New layer added in solution, but not implemented

---
+++ Gallery.ThirdBody/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.ThirdBody
+{
+    public class Class1
+    {
+        Class1()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
