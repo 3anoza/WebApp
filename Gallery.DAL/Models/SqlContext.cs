da57efc15fe0148cea9ff238c1e23e9f0d800b6a
Yeremeev <tonaros.kill@gmail.com>
2020-06-05 17:30:37 +0300

    Rename UserContext to SqlContext

---
+++ Gallery.DAL/Models/SqlContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+
+namespace Gallery.DAL.Models
+{
+    public class SqlContext: DbContext
+    {
+        public SqlContext()
+        { }
+
+        public SqlContext(string connectionString) : base(connectionString) 
+        { }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Configurations.Add(new Configuration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
