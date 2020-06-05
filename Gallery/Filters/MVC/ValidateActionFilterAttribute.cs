7564dc6b67812548b57d2fff5e6163480ed3d97c
Yeremeev <tonaros.kill@gmail.com>
2020-06-05 17:01:18 +0300

    added filter returning 400th error if ModelState is not correct

---
+++ Gallery/Filters/MVC/ValidateActionFilterAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace Gallery.Filters.MVC
+{
+    public class ValidateActionFilterAttribute: ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var isValid = filterContext.Controller.ViewData.ModelState.IsValid;
+            if (!isValid)
+                filterContext.RequestContext.HttpContext.Response.StatusCode = 400;
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
