d2ea854e1c46a65772e7d98b2561fa7a63660e70
Yeremeev <tonaros.kill@gmail.com>
2020-06-05 17:26:13 +0300

    role model added

---
+++ Gallery.DAL/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Gallery.DAL.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<User> Users { get; set; } = new List<User>();
+    }
+}
