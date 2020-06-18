da979667d59eb8ac46a9fe0f5ed0d28e73b1cd31
Yeremeev <tonaros.kill@gmail.com>
2020-06-18 22:25:58 +0300

    add web config class

---
+++ Gallery/ConfigManagment/ConfigurationManager.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace Gallery.ConfigManagment
+{
+    public class ConfigurationManager
+    {
+        public string FileExtensions { get; private set; } = "FileExtensions";
+        private string DefaultFileExtensions { get; set; } = "image/jpeg;image/png";
+
+        public string IsFileExtensionsExist()
+        {
+            var appSettings = System.Configuration.ConfigurationManager.AppSettings;
+            var types = appSettings[FileExtensions];
+            
+            if (string.IsNullOrEmpty(types))
+            {
+                types = DefaultFileExtensions;
+            }
+
+            return types;
+        }
+        
+        public static string SqlDbConnectionString()
+        {
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["SQLCONN"] ??
+                                   throw new ArgumentException("SQLCONN");
+            return connectionString.ConnectionString;
+        }
+    }
+}
