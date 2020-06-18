2fff1d25a881c216c625db364f80ddfda3e73120
Yeremeev <tonaros.kill@gmail.com>
2020-06-18 22:27:44 +0300

    add database initializer

--- Gallery/Global.asax.cs
+++ Gallery/Global.asax.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Gallery.DAL.Contexts;
 
 namespace Gallery
 {
@@ -15,6 +17,7 @@ namespace Gallery
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BoundleConfig.RegisterBoundles(BundleTable.Bundles);
+            Database.SetInitializer(new UserDbInitializer());
         }
     }
 }
