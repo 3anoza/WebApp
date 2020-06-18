bef32e812b97bac623d4fd26d04f62b46076928a
Yeremeev <tonaros.kill@gmail.com>
2020-06-18 22:33:17 +0300

    context class has been moved to the appropriate folder

---
+++ Gallery.DAL/Contexts/SqlContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.Entity;
+using Gallery.DAL.Models;
+
+namespace Gallery.DAL.Contexts
+{
+    public class SqlContext: DbContext
+    {
+        public SqlContext()
+        { }
+
+        public SqlContext(string connectionString) : base(connectionString) 
+        { }
+        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        /*protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Configurations.Add(new Configuration());
+            base.OnModelCreating(modelBuilder);
+        }*/
+    }
+
+    public class UserDbInitializer : DropCreateDatabaseAlways<SqlContext>
+    {
+        protected override void Seed(SqlContext context)
+        {
+            context.Roles.Add(new Role { Id = 1, Name = "admin"});
+            context.Roles.Add(new Role { Id = 2, Name = "user"});
+            base.Seed(context);
+        }
+    }
+}
