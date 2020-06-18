35063b1efeb778550cf1e3dc6b8d9f5d44d6dc7e
Yeremeev <tonaros.kill@gmail.com>
2020-06-18 22:47:39 +0300

    added table of connection attempts

--- Gallery.DAL/Models/Attempts.cs
+++ Gallery.DAL/Models/Attempts.cs
@@ -1,7 +1,19 @@
-﻿namespace Gallery.DAL.Models
+﻿using System;
+
+namespace Gallery.DAL.Models
 {
     public class Attempts
     {
+        public long Id { get; private set; }
         
+        public DateTime TimeStamp { get; private set; }
+        
+        public bool Success { get; private set; }
+        
+        public string IpAddress { get; private set; }
+        
+        public int UserId { get; private set; }
+        
+        public virtual User User { get; private set; }
     }
 }
