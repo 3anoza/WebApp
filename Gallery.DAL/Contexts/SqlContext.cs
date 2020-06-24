7f44995460ddafa94bb37cf03d6912a0f3b6e4cf
Yeremeev <tonaros.kill@gmail.com>
2020-06-24 11:04:41 +0300

    Fluent API added in SqlContext

--- Gallery.DAL/Contexts/SqlContext.cs
+++ Gallery.DAL/Contexts/SqlContext.cs
@@ -14,11 +14,35 @@ namespace Gallery.DAL.Contexts
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
 
-        /*protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        // Fluent API method
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            //modelBuilder.Configurations.Add(new Configuration());
+            // Mapping a class to a table
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<Role>().ToTable("Roles");
+            modelBuilder.Entity<Media>().ToTable("Media");
+            modelBuilder.Entity<MediaType>().ToTable("MediaTypes");
+
+            // Set own primary key
+            modelBuilder.Entity<User>().HasKey(p => p.Id);
+            modelBuilder.Entity<Role>().HasKey(p => p.Id);
+            modelBuilder.Entity<Media>().HasKey(p => p.Id);
+            modelBuilder.Entity<MediaType>().HasKey(p => p.Id);
+            
+            // Not Null extensions
+            modelBuilder.Entity<User>().Property(p => p.Id).IsRequired();
+            modelBuilder.Entity<User>().Property(p => p.Email).IsRequired();
+            modelBuilder.Entity<User>().Property(p => p.Password).IsRequired();
+            
+            // Set column length limit
+            modelBuilder.Entity<User>().Property(p => p.Email).HasMaxLength(64);
+            modelBuilder.Entity<User>().Property(p => p.Password).HasMaxLength(38);
+            modelBuilder.Entity<Role>().Property(p => p.Name).HasMaxLength(16);
+            modelBuilder.Entity<Media>().Property(p => p.Path).HasMaxLength(25);
+            modelBuilder.Entity<MediaType>().Property(p => p.Type).HasMaxLength(6);
+
             base.OnModelCreating(modelBuilder);
-        }*/
+        }
     }
 
     public class UserDbInitializer : DropCreateDatabaseAlways<SqlContext>
