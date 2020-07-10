d1afa5aa55a38110c889ef233eb457f0f7dc0b2b
Yeremeev <tonaros.kill@gmail.com>
2020-07-10 14:48:00 +0300

    new table for database added

---
+++ Gallery.BLL/Contracts/AttemptsDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Gallery.BLL.Contracts
+{
+    public class AttemptsDTO
+    {
+        public string Email { get; set; }
+        public string  IpAddress { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}
