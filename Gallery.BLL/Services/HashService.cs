d472c9881f8e7464d70a88d4c4c3b00194d3d195
Yeremeev <tonaros.kill@gmail.com>
2020-07-10 14:56:12 +0300

    Hash service impementation added

--- Gallery.BLL/Services/HashService.cs
+++ Gallery.BLL/Services/HashService.cs
@@ -1,13 +1,26 @@
-﻿using Gallery.BLL.Interfaces;
+﻿using System.Security.Cryptography;
+using System.Text;
+using Gallery.BLL.Interfaces;
 
 namespace Gallery.BLL.Services
 {
     public class HashService : IHashService
     {
-        public bool CompareHashes(string imgHash)
+        public string CompareSha256Hashes(string data)
         {
-            //empty now
-            return false;
+            using (SHA256 hashSha256 = SHA256.Create())
+            {
+                byte[] bytes = hashSha256.ComputeHash(Encoding.UTF8.GetBytes(data));
+
+                StringBuilder builder = new StringBuilder();
+
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
         }
     }
 }
