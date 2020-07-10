b69827602122f43568de1fbe6bdebd09dd16a48a
Yeremeev <tonaros.kill@gmail.com>
2020-07-10 14:58:04 +0300

    Added impementation of a new name generator for files

--- Gallery.BLL/Services/ImageService.cs
+++ Gallery.BLL/Services/ImageService.cs
@@ -1,9 +1,48 @@
-﻿using Gallery.BLL.Interfaces;
+﻿using System;
+using System.Text.RegularExpressions;
+using Gallery.BLL.Interfaces;
+using Gallery.FileStorage.Interfaces;
 
 namespace Gallery.BLL.Services
 {
     public class ImageService : IImageService
     {
-        
+        protected  readonly IMediaStorage _mediaStorage;
+        public ImageService(IMediaStorage mediaStorage)
+        {
+            _mediaStorage = mediaStorage ?? throw new ArgumentNullException();
+        }
+
+        public bool Upload(byte[] bytes, string path)
+        {
+            return _mediaStorage.Upload(bytes, path);
+        }
+
+        public byte[] Read(string path)
+        {
+            return _mediaStorage.Read(path);
+        }
+
+        public string FilenameGenerator(string filename)
+        {
+            try
+            {
+                return Regex.Replace
+                (filename,
+                    @"[^\w\.@-]",
+                    "",
+                    RegexOptions.None,
+                    TimeSpan.FromSeconds(1.5));
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                return "[ERR]: " + e.ToString();
+            }
+        }
+
+        public bool Delete(string path)
+        {
+            return _mediaStorage.Delete(path);
+        }
     }
 }
