e972d1814a2ebd1d439908a785c06d29ba13bd73
Yeremeev <tonaros.kill@gmail.com>
2020-07-10 14:46:57 +0300

    AddUserDto logic moved to UserDto class

--- Gallery.BLL/Contracts/UserDto.cs
+++ Gallery.BLL/Contracts/UserDto.cs
@@ -1,8 +1,13 @@
-﻿namespace Gallery.BLL.Contracts
+﻿using System.Collections.Generic;
+using Gallery.DAL.Models;
+
+namespace Gallery.BLL.Contracts
 {
     public class UserDto
     {
-        public int PersonID { get; set; }
-        public string Username { get; set; }
+        public int UserId { get; protected set; }
+        public string UserEmail { get; protected set; }
+        public string Password { get; protected set; }
+        public List<Role> UserRole { get; protected set; }
     }
 }
