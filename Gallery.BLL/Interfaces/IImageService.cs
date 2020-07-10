a20767f447f72843065e2b3fffd02722dfe7b89d
Yeremeev <tonaros.kill@gmail.com>
2020-07-10 14:51:39 +0300

    added method for generating a new name, methods description

--- Gallery.BLL/Interfaces/IImageService.cs
+++ Gallery.BLL/Interfaces/IImageService.cs
@@ -2,6 +2,30 @@
 {
     public interface IImageService
     {
-        
+        /// <summary>
+        /// The method writes an array of bytes to a file.
+        /// </summary>
+        /// <param name="bytes">file in byte array</param>
+        /// <param name="path">path to the saved directory</param>
+        /// <returns>Execution result</returns>
+        bool Upload(byte[] bytes, string path);
+        /// <summary>
+        /// The method deletes the file at the given path 
+        /// </summary>
+        /// <param name="path">path to file</param>
+        /// <returns>Execution result</returns>
+        bool Delete(string path);
+        /// <summary>
+        /// The method read all bytes from the given path
+        /// </summary>
+        /// <param name="path">path to file</param>
+        /// <returns>file in byte array</returns>
+        byte[] Read(string path);
+        /// <summary>
+        /// The method for generating a new name for the transferred file
+        /// </summary>
+        /// <param name="filename">current file name</param>
+        /// <returns>New file name</returns>
+        string FilenameGenerator(string filename);
     }
 }
