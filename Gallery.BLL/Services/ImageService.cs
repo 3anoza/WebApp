911425c20994d7ae94229f2e0d29b84b35b8d85f
Yeremeev <tonaros.kill@gmail.com>
2020-07-15 17:33:36 +0300

    updated implementation of methods according to the new interface structure

--- Gallery.BLL/Services/ImageService.cs
+++ Gallery.BLL/Services/ImageService.cs
@@ -1,20 +1,41 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Gallery.BLL.Contracts;
 using Gallery.BLL.Interfaces;
 using Gallery.FileStorage.Interfaces;
+using Gallery.DAL.Interfaces;
 
 namespace Gallery.BLL.Services
 {
     public class ImageService : IImageService
     {
-        protected  readonly IMediaStorage _mediaStorage;
-        public ImageService(IMediaStorage mediaStorage)
+        protected readonly IMediaStorage _mediaStorage;
+        protected readonly IMediaRepository _mediaRepository;
+        protected readonly IRepository _repository;
+        public ImageService(IMediaStorage mediaStorage, IMediaRepository mediaRepository, IRepository repository)
         {
-            _mediaStorage = mediaStorage ?? throw new ArgumentNullException();
+            _mediaStorage = mediaStorage ?? throw new ArgumentNullException(nameof(_mediaStorage));
+            _mediaRepository = mediaRepository ?? throw new ArgumentNullException(nameof(_mediaRepository));
+            _repository = repository ?? throw new ArgumentNullException(nameof(_repository));
         }
 
-        public bool Upload(byte[] bytes, string path)
+        public async Task<bool> UploadImageAsync(byte[] bytes, string path, UserDto userDto)
         {
+            var IsMediaExistAsync = await _mediaRepository.IsMediaExistAsync(path);
+            if (IsMediaExistAsync)
+            {
+                var media = await _mediaRepository.GetMediaByPathAsync(path);
+                if (media.IsDeleted)
+                {
+                    await _mediaRepository.MediaDeletedStatusUpdateAsync(path, false);
+                }
+            }
+
+            var extension = Path.GetExtension(path);
+            var user = await _repository.FindUserAsync(userDto.UserEmail, userDto.Password);
             return _mediaStorage.Upload(bytes, path);
         }
 
@@ -40,8 +61,13 @@ namespace Gallery.BLL.Services
             }
         }
 
-        public bool Delete(string path)
+        public async Task<bool> DeleteImageAsync(string path)
         {
+            var IsMediaExistAsync = await _mediaRepository.IsMediaExistAsync(path);
+            if (IsMediaExistAsync)
+            {
+                await _mediaRepository.MediaDeletedStatusUpdateAsync(path, true);
+            }
             return _mediaStorage.Delete(path);
         }
     }
