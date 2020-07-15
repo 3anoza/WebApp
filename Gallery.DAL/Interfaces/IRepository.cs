6bd4b3c0670b2a900899d0373ffd801e4f4bac84
Yeremeev <tonaros.kill@gmail.com>
2020-07-15 17:48:16 +0300

    method GetUserByIdAsync added to interface structure

--- Gallery.DAL/Interfaces/IRepository.cs
+++ Gallery.DAL/Interfaces/IRepository.cs
@@ -7,9 +7,11 @@ namespace Gallery.DAL.Interfaces
     {
         Task<bool> IsUserExistAsync(string userEmail, string password);
         Task<User> FindUserAsync(string email, string password);
+        Task<bool> IsUserExistByEmailAsync(string email);
+        Task<User> GetUserByIdAsync(int id);
         Task AddUserToDatabaseAsync(string userEmail, string password);
         Task AddAttemptToDatabaseAsync(string email, string ipAddress, bool isSuccess);
         int GetPersonId(string userEmail);
-        string GetUsersNames(int id);
+        
     }
 }
