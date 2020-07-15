3e84571edb3308c9b377c3180d86d36529ea27e3
Yeremeev <tonaros.kill@gmail.com>
2020-07-15 17:44:19 +0300

    added new model for work with ClaimIdentity

---
+++ Gallery.DAL/Models/Identity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace Gallery.DAL.Models
+{
+    public static class Identity
+    {
+        public static string GetUserRole(this IIdentity identity)
+        {
+            if (identity == null)
+            {
+                throw new ArgumentNullException(nameof(identity));
+            }
+
+            var ci = identity as ClaimsIdentity;
+            string role = String.Empty;
+            var claim = ci?.FindFirst(ClaimsIdentity.DefaultRoleClaimType);
+            if (claim != null)
+            {
+                role = claim.Value;
+            }
+
+            return role;
+        }
+    }
+}
