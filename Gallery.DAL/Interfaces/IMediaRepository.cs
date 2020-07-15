66fd86b8efa5aa13d5abcbcf1aaa3f85edecf160
Yeremeev <tonaros.kill@gmail.com>
2020-07-15 17:50:09 +0300

    added a new interface whose methods work with media

---
+++ Gallery.DAL/Interfaces/IMediaRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gallery.DAL.Models;
+
+namespace Gallery.DAL.Interfaces
+{
+    public interface IMediaRepository
+    {
+        Task<bool> IsMediaExistAsync(string path);
+        Task MediaDeletedStatusUpdateAsync(string path,bool status);
+        Task AddMediaToDatabaseAsync(string name, string path, int userId, int mediaTypeId);
+        Task<Media> GetMediaByPathAsync(string path);
+        Task<bool> IsMediaTypeExistAsync(string extension);
+        Task AddMediaTypeToDatabaseAsync(string type);
+        Task<MediaType> GetMediaTypeAsync(string extension);
+    }
+}
