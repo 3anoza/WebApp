f2cb507584e0f31ed768e08ba81a7a345e3e5529
Yeremeev <tonaros.kill@gmail.com>
2020-07-15 17:35:58 +0300

    added IsUserExistByEmailAsync and GetUserByIdAsync to interface

--- Gallery.BLL/Interfaces/IUserService.cs
+++ Gallery.BLL/Interfaces/IUserService.cs
@@ -8,6 +8,8 @@ namespace Gallery.BLL.Interfaces
     {
         Task<bool> IsUserExistAsync(UserDto userDto);
         Task<UserDto> FindUserAsync(UserDto userDto);
+        Task<bool> IsUserExistByEmailAsync(UserDto userDto);
+        Task<UserDto> GetUserByIdAsync(int id);
         Task AddUserAsync(UserDto userDto);
         Task AddAttemptAsync(AttemptsDTO attemptsDto);
         int GetPersonId(string username);
