f9e34c3c4fa5d6c1946f97f079e80970dba55787
Yeremeev <tonaros.kill@gmail.com>
2020-07-15 17:31:40 +0300

    prop setters were announced publicly

--- Gallery.BLL/Contracts/UserDto.cs
+++ Gallery.BLL/Contracts/UserDto.cs
@@ -5,9 +5,9 @@ namespace Gallery.BLL.Contracts
 {
     public class UserDto
     {
-        public int UserId { get; protected set; }
-        public string UserEmail { get; protected set; }
-        public string Password { get; protected set; }
-        public List<Role> UserRole { get; protected set; }
+        public int UserId { get;  set; }
+        public string UserEmail { get;  set; }
+        public string Password { get;  set; }
+        public List<Role> UserRole { get;  set; }
     }
 }
